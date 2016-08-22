6f965e82eb1120227e5779e1ffc45d9b8927a902
Erikang20 <erikang20@gmail.com>
2016-08-22 14:31:45 -0500

    add changes

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-// var express = require( 'express' );
-// var path = require( 'path' );
-// var routes = require( './index' );
-// var app = express();
-//
-//
-//
-// app.use( './', routes );
-//
-// var port = process.env.PORT || 3000;
-// app.listen( port, function () {
-//     console.log( "Im listening yo!" );
-// } );
-//
-// module.exports = {
-//     app
-// };
+var express = require( 'express' );
+var path = require( 'path' );
+var bodyParser = require( 'body-parser' );
+
+var routes = require( './routes/index' );
+var app = express();
+
+
+app.set( 'view engine', 'ejs' );
+app.set( 'views', path.join( __dirname, '/views' ) );
+app.use( express.static( path.join( __dirname, '/public' ) ) );
+app.use( logger( 'dev' ) );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( {
+    extended: false
+} ) );
+
+app.use( './', routes );
+
+app.get( '/', function ( req, res ) {
+    res.render( 'index' )
+} );
+
+var port = process.env.PORT || 3000;
+app.listen( port, function () {
+    console.log( "Im listening yo!" );
+} );
+
+module.exports = {
+    app
+};
