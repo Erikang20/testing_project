1bbf68a667f4d156161aaae37e9b3f82b592b9d3
Erikang20 <erikang20@gmail.com>
2016-08-22 15:58:33 -0500

    add get

--- test.js
+++ test.js
@@ -5,4 +5,15 @@ describe( 'testing connection', function () {
     it( 'should return true if there is connection', function () {
         expect( suite.connected() ).to.equal( true );
     } );
+
+} );
+
+describe( 'GET /', function () {
+    it( 'should 'function ( done ) {
+        request.get( '/index' ).expect( 200 )
+            .end( function ( err, res ) {
+                if ( err ) return done( err )
+                done();
+            } );
+    } );
 } );
