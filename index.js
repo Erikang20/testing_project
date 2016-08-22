8f801f048bdf50f13ad35e818e26212abdf08d65
Erikang20 <erikang20@gmail.com>
2016-08-22 14:37:28 -0500

    add favicon

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require( 'express' );
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
 var logger = require( 'morgan' );
+var favicon = require( 'serve-favicon' );
 
 var routes = require( './routes/index' );
 var app = express();
@@ -9,7 +10,7 @@ var app = express();
 
 app.set( 'view engine', 'ejs' );
 app.set( 'views', path.join( __dirname, '/views' ) );
-app.use( express.static( path.join( __dirname, '/public' ) ) );
+// app.use( express.static( path.join( __dirname, '/public' ) ) );
 app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( {
