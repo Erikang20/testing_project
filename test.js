210d98d479c7b52250e66042d5df639d31b1f35a
Erikang20 <erikang20@gmail.com>
2016-08-22 12:39:02 -0500

    add test

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-var suite = require( './implementation' );
+var suite = require( './app' );
 var expect = require( 'chai' ).expect;
+
+describe( 'testing connection', function () {
+    it( 'should return true if there is connection', function () {
+        expect( suite.connected() ).to.equal( true );
+    } );
+} );
