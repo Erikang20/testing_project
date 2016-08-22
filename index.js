203073cefae2d2294438fb1e6bbe34db90c2eccd
Erikang20 <erikang20@gmail.com>
2016-08-22 14:21:46 -0500

    add changes

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var express = require( 'express' );
-
-
-var port = process.env.PORT || 3000;
-app.listen( port, function () {
-    console.log( "Im listening yo!" );
-} );
-
-module.exports = {
-    app
-};
+// var express = require( 'express' );
+// var path = require( 'path' );
+// var routes = require( './index' );
+// var app = express();
+//
+//
+//
+// app.use( './', routes );
+//
+// var port = process.env.PORT || 3000;
+// app.listen( port, function () {
+//     console.log( "Im listening yo!" );
+// } );
+//
+// module.exports = {
+//     app
+// };
